64fbf892c5daaf3da485033c75610808413c34b1
John Galindez <jfrankgalindez@sipay.es>
2019-02-18 17:49:16 +0100

    installing bootstrap and adding only the necessary

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <WebcompsButton>Hola</WebcompsButton>
-    <WebcompsCard/>
+    <div class="container">
+      <div class="col-12">
+        <section class="d-flex flex-column align-items-baseline">
+          <h1>Buttons</h1>
+          <WebcompsButton>Default Button</WebcompsButton>
+        </section>
+        <section class="mt-4">
+           <h1 class="text-left">Cards</h1>
+           <WebcompsCard/>
+        </section>
+        <p class="text-center mt-5">More components soon ...</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,6 +22,7 @@ export default {
 }
 </script>
 
+<style lang="scss" src="./assets/styles/main.scss"></style>
 <style src="@jfdev/webcomps-button/dist/webcomps-button.css"></style>
 <style src="@jfdev/webcomps-card/dist/webcomps-card.css"></style>
 <style>
